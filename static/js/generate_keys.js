b76c60df688f56f32e131cbe2d188d1f602203b6
Dominic Lakshan <dominiclakshank@gmail.com>
2024-09-17 14:45:51 +0530

    Adding select functionality for generate keys

--- static/js/generate_keys.js
+++ static/js/generate_keys.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
-    console.log("generate_keys.js file loaded")
+    function updateKeySizes(algorithmSelector, keySizeSelector) {
+        const selectedAlgorithm = $(algorithmSelector).find(':selected');
+        const keySizesJson = selectedAlgorithm.attr('data-key-sizes');
+
+        if (keySizesJson) {
+            try {
+                const keySizesArray = JSON.parse(keySizesJson).map(Number);
+                $(keySizeSelector).empty();
+
+                keySizesArray.forEach(function (size) {
+                    $(keySizeSelector).append($('<option>', {
+                        value: size,
+                        text: size.toString()
+                    }));
+                });
+            } catch (error) {
+                console.error("Error parsing key sizes:", error);
+            }
+        } else {
+            console.error("No key sizes found for the selected algorithm.");
+        }
+    }
+
+    $('#zsk-algorithm').on('change', function () {
+        updateKeySizes('#zsk-algorithm', '#zsk-key-size');
+    });
+
+    $('#ksk-algorithm').on('change', function () {
+        updateKeySizes('#ksk-algorithm', '#ksk-key-size');
+    });
+
+    // Initially populate the key sizes for both ZSK and KSK
+    updateKeySizes('#zsk-algorithm', '#zsk-key-size');
+    updateKeySizes('#ksk-algorithm', '#ksk-key-size');
 })
