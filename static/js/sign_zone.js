875865525eecbbaa93287291ddf38877a63491e6
Dominic Lakshan <dominiclakshank@gmail.com>
2024-09-18 13:07:35 +0530

    adding btn disability function to sign zone

--- static/js/sign_zone.js
+++ static/js/sign_zone.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-    console.log("sign_zone.js file loaded")
+    function checkInputs() {
+        const origin = $('#sign-zone-origin').val();
+        const keysDirectory = $('#sign-zone-keys-directory').val();
+        const zoneFilePath = $('#sign-zone-file-path').val();
+
+        if (origin !== '' && keysDirectory !== '' && zoneFilePath !== '') {
+            $('#sign-zone-btn').prop('disabled', false);
+        } else {
+            $('#sign-zone-btn').prop('disabled', true);
+        }
+
+    }
+
+    $('#sign-zone-origin, #sign-zone-keys-directory, #sign-zone-file-path').on('input change', checkInputs);
+
+    checkInputs();
 })
