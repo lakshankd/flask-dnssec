3da7021ebab782b2497130b3f7f82b03d22defb5
Dominic Lakshan <dominiclakshank@gmail.com>
2024-09-28 22:09:29 +0530

    adding apply changes info para and its' styles

--- static/js/apply_changes.js
+++ static/js/apply_changes.js
@@ -13,7 +13,34 @@ $(document).ready(function () {
     $('#zone-file').on('input change', checkInputs);
 
     checkInputs();
-    
+
+    $('#apply-changes-btn').click(function () {
+        const zoneFile = $('#zone-file').val();
+
+        $.ajax({
+            url: applyChangesRequestUrl,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({zone_name: zoneFile}),
+            success: function (response) {
+                console.log("success function called")
+                $('.apply-changes-info')
+                    .addClass('success-backup-message')
+                    .html(`<i class="fa fa-check-circle"></i> ${response.message}`)
+                    .show();
+            },
+            error: function (xhr) {
+                console.log("error function called")
+                const errorResponse = xhr.responseJSON ? xhr.responseJSON.error : 'An error occurred';
+                $('.apply-changes-info')
+                    .addClass('error-backup-message')
+                    .html(`<i class="fa fa-exclamation-circle"></i> ${errorResponse}`)
+                    .show();
+                console.error(errorResponse);
+            }
+        });
+    });
+
     $('#next-view-statistics-btn').click(function () {
         window.location.href = viewStatisticsUrl;
     });
